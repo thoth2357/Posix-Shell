e8799cee6b9e51c30e01fb03835f4cc26836b953
thoth2357 <thoth2357@gmail.com>
2024-12-08 02:09:11 +0100

    ✨  Implemented type builtins to the shell

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from subprocess import Popen, PIPE, STDOUT
 
 
 def main():
@@ -9,15 +10,21 @@ def main():
     # Wait for user input
     while (command := input("$ ")):
         
-        if "exit" ==  command.split(" ")[0]:
-            sys.exit(int(command.split(" ")[-1]))
+        if "exit" == command.split(" ")[0]:
+            sys.exit(int(command.split(" ")[-1])) if len(command) == 2 else sys.exit(0)
             break
         
         elif command.split(" ")[0] == "echo":
             sys.stdout.write(f"{command.removeprefix('echo').strip()}\n")
+        elif command.split(" ")[0] == "type":
+            if "not" in str(Popen(f"type {command.split(' ')[-1]}", shell=True, stdout=PIPE, stderr=STDOUT).communicate()[0]).split():
+                sys.stdout.write(f"{command.split(' ')[-1]} not found\n")
+            else:
+                sys.stdout.write(f"{command.split(' ')[-1]} is a shell builtin\n")
+
         else:
             # implementing the handler to handle invalid commands
-            sys.stdout.write(f"{command}: command not found\n")
+            sys.stdout.write(f"{command.split(' ')[-1]}: command not found\n")
         
         
         # os.system(command:= input())
